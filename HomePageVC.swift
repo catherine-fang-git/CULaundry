a33811e42d9972da8051a58f3c63301090b969f0
catherine-fang-git <157651589+catherine-fang-git@users.noreply.github.com>
2024-04-29 12:19:34 -0400

    does this work

---
+++ HomePageVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  CU Laundry
+//
+//  Created by Catherine Fang on 4/29/24.
+//
+
+import Foundation
+import UIKit
+
+
+class HomePageVC: UIViewController {
+    // MARK: - Properties (view)
+    
+    // MARK: - Properties (data)
+    
+    // MARK: - ViewDidLoad + init
+    
+    // MARK: - Set Up Views
+}
+
+
+
+    // MARK: - UICollectionViewDelegate
+
+
+    // MARK: - UICollectionViewDataSource
+
+
+    // MARK: - UICollectionViewDelegateFlowLayout
---
+++ WasherStatusViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  WasherStatusViewCell.swift
+//  CU Laundry
+//
+//  Created by Catherine Fang on 4/29/24.
+//
+
+import Foundation
+import UIKit
+
+class WasherStatusViewCell: UICollectionViewCell {
+    // MARK: - Properties (view)
+    
+    // MARK: - Properties (data)
+    
+    // MARK: - ViewDidLoad + init
+    
+    // MARK: - Configure
+    
+    // MARK: - Set Up Views
+}
